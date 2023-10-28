b477d7a2cb1d144465518760fc6a89958e567f94
Takayuki Ono <takaaaaadayo@gmail.com>
2023-10-28 16:16:57 +0530

    first set

--- packages/contracts/mud.config.ts
+++ packages/contracts/mud.config.ts
@@ -2,9 +2,80 @@ import { mudConfig } from "@latticexyz/world/register";
 
 export default mudConfig({
   tables: {
-    Counter: {
-      keySchema: {},
-      valueSchema: "uint32",
+    Chain: {
+      keySchema: {
+        chainId: "bytes32",
+      },
+      valueSchema: {
+        chainGasFee: "uint16",
+        landId: "uint8",
+        chainName: "string",
+        chainColor: "string",
+      },
+    },
+
+    Land: {
+      keySchema: {
+        landId: "uint8",
+      },
+      valueSchema: {
+        pixelId: "uint8",
+      },
+    },
+
+    PixelColor: {
+      keySchema: {
+        landId: "uint8",
+        pixelId: "uint8",
+      },
+      valueSchema: {
+        r: "uint8",
+        g: "uint8",
+        b: "uint8",
+        a: "uint8",
+      },
+    },
+
+    PixelAddress: {
+      keySchema: {
+        landId: "uint8",
+        pixelId: "uint8",
+      },
+      valueSchema: {
+        address: "address",
+      },
+    },
+
+    PixelAddressConnection: {
+      keySchema: {
+        landId: "uint8",
+        pixelId: "uint8",
+      },
+      valueSchema: {
+        connectedLandId: "uint8",
+        connectedPixelId: "uint8",
+      },
+    },
+
+    AddressInfo: {
+      keySchema: {
+        address: "address",
+      },
+      valueSchema: {
+        mainCategory: "string",
+        subCategory: "string",
+        name: "string",
+        description: "string",
+      },
+    },
+
+    AddressBoard: {
+      keySchema: {
+        address: "address",
+      },
+      valueSchema: {
+        chat: "bytes32", // convert string to bytes32 to store
+      },
     },
   },
 });
