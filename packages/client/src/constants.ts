fb1e145f9a0c12f520e6277d82d246d923d5c400
Takayuki Ono <takaaaaadayo@gmail.com>
2023-10-28 14:09:49 +0530

    add kind of chain

--- packages/client/src/constants.ts
+++ packages/client/src/constants.ts
@@ -5,4 +5,16 @@ export const PRESETCOLORS = [
     "#303F9F", "#1976D2", "#0288D1", "#0097A7",
     "#00796B", "#388E3C", "#689F38", "#AFB42B",
     "#FBC02D", "#FFA000", "#F57C00", "#E64A19"
-  ];
+];
+
+export const chains = [
+  { name: 'Ethereum', id: 0x1,color: '#121212' },
+  { name: 'OP+mainnet', id: 0xa, color: '#FC0721' },
+  { name: 'Base', id: 0x2105,color: '#799CF5' }, 
+  { name: 'BNB', id: 0x38, color: '#F0B90B' },
+  { name: 'Polygon+PoS', id: 0x89, color: '#8C44ED' },
+  { name: 'Avalanche+C-chain', id: 0xa86a, color: '#E84440' },
+  { name: 'Celo', id: 0xa4ec, color: '#809863' },
+  { name: 'Gnosis', id: 0x64, color: '#11765B' },
+  { name: 'Arbitrum+One', id: 0xa4b1,color: '#2050DE' },
+];
